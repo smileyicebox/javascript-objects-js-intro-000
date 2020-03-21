f3fdc2e23945a5d8f29da8191012bbe8c780aec7
Jon Lewis <lewis.jonathan.n@gmail.com>
2020-03-21 15:35:12 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {creed: 'Higher'}
 function updatePlaylist(playlist, artistName, songTitle){
   return Object.assign(playlist, { [artistName]: songTitle})
 }
+
+function removeFromPlaylist(playlist, artistname) {
+  
+}
